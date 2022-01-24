f1caf2371969131ed74e2dc80e3be2c22071cfe8
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2022-01-24 18:56:53 +0100

    Style fixes

--- lua/autorun/client/boom_headshot.lua
+++ lua/autorun/client/boom_headshot.lua
@@ -1,34 +1,38 @@
 -- These can be accessed without pointing to the IMaterial by using ! before the material string.
 function CreateSpriteMaterials()
-	local params = {["$translucent"] = "1", ["$vertexcolor"] = "1", ["$vertexalpha"] = "1"}
-	for i=1, 8 do
-		params["$basetexture"] = "Decals/blood"..i
-		CreateMaterial("sprite_bloodspray"..i, "UnlitGeneric", params)
-	end
+    local params = {
+        ["$translucent"] = "1",
+        ["$vertexcolor"] = "1",
+        ["$vertexalpha"] = "1"
+    }
+
+    for i = 1, 8 do
+        params["$basetexture"] = "Decals/blood" .. i
+        CreateMaterial( "sprite_bloodspray" .. i, "UnlitGeneric", params )
+    end
 end
 
-hook.Add("Initialize","CreateBlood.TTT", function()
-	CreateSpriteMaterials()
-end)
+hook.Add( "Initialize", "CreateBlood.TTT", function()
+    CreateSpriteMaterials()
+end )
 
 hook.Remove( "CreateClientsideRagdoll", "FWKZT.Headshot.CreateCRagdolls" )
+
 hook.Add( "CreateClientsideRagdoll", "FWKZT.Headshot.CreateCRagdolls", function( entity, ragdoll )
-	if GAMEMODE_NAME ~= "sandbox" then return end
-	
-	if entity.WasHeadshotDeath and entity:WasHeadshotDeath() then
-		local Head = ragdoll:LookupBone('valvebiped.bip01_head1')
-		if !Head then return end
-		local Pos = ragdoll:GetBonePosition( Head )
+    if GAMEMODE_NAME ~= "sandbox" then return end
 
-		local RagHead = ragdoll:LookupBone('valvebiped.bip01_head1')
-		if !RagHead then return end
-		ragdoll:ManipulateBoneScale(RagHead, Vector(0,0,0) )
-
-		local ED = EffectData()
-			ED:SetEntity( entity )
-			ED:SetNormal( -entity:GetForward() )
-			ED:SetScale( entity:EntIndex() )
-			ED:SetOrigin( Pos )
-		util.Effect( 'headshot', ED )
-	end
+    if entity.WasHeadshotDeath and entity:WasHeadshotDeath() then
+        local Head = ragdoll:LookupBone( "valvebiped.bip01_head1" )
+        if not Head then return end
+        local Pos = ragdoll:GetBonePosition( Head )
+        local RagHead = ragdoll:LookupBone( "valvebiped.bip01_head1" )
+        if not RagHead then return end
+        ragdoll:ManipulateBoneScale( RagHead, Vector( 0, 0, 0 ) )
+        local ED = EffectData()
+        ED:SetEntity( entity )
+        ED:SetNormal( -entity:GetForward() )
+        ED:SetScale( entity:EntIndex() )
+        ED:SetOrigin( Pos )
+        util.Effect( "headshot", ED )
+    end
 end )
