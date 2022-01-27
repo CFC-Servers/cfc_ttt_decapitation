9b3400e29ad0f0dbc94eb44dbd67aeed3e8497ce
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2022-01-27 04:57:49 +0100

    Lower angular force to fix crazyphysics spam

--- lua/effects/headshot.lua
+++ lua/effects/headshot.lua
@@ -51,7 +51,7 @@ function EFFECT:Init( data )
 
             if phys:IsValid() then
                 phys:SetMaterial( "zombieflesh" )
-                phys:ApplyForceOffset( ent:GetPos() + VectorRand() * 5, dir * math.Rand( 300, 800 ) )
+                phys:ApplyForceOffset( ent:GetPos() + VectorRand() * 5, dir * math.Rand( 50, 100 ) )
             end
 
             SafeRemoveEntityDelayed( ent, math.Rand( 6, 10 ) )
