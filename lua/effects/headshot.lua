2c7c1d0808b6efcad6b1953b61b78cd02f7e579a
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2022-01-27 05:25:57 +0100

    Lower force even more to prevent crazy physics

--- lua/effects/headshot.lua
+++ lua/effects/headshot.lua
@@ -51,7 +51,7 @@ function EFFECT:Init( data )
 
             if phys:IsValid() then
                 phys:SetMaterial( "water" )
-                phys:ApplyForceOffset( ent:GetPos() + VectorRand() * 5, dir * math.Rand( -10, 10 ) )
+                phys:ApplyForceOffset( ent:GetPos() + VectorRand() * 5, dir * math.Rand( -5, 5 ) )
             end
 
             SafeRemoveEntityDelayed( ent, math.Rand( 6, 10 ) )
